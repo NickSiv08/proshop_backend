8598adee41c8cefd8b68bf5bd48f98561ab517cb
giannos75 <giasivros@gmail.com>
2022-10-24 15:58:21 +0300

    Final Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true, 
-            useNewUrlParser: true,
-        })
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
-        console.error(`Error: ${error.message}`.red.bold)
-        process.exit(1)
-    }
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold)
+    process.exit(1)
+  }
 }
 
 export default connectDB
