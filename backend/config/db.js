c9efd661bab25071156f34c5119ba0693cdbb13e
giannos75 <giasivros@gmail.com>
2022-10-24 16:00:38 +0300

    Final Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,8 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
+      useNewUrlParser: true,
     })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
