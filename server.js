390289b1cef4ebdce1e58d752a9cd2c3be3da65d
giannos75 <giasivros@gmail.com>
2022-10-24 16:42:36 +0300

    Final Commit

---
+++ server.js
@@ -0,0 +1,59 @@
+import path from 'path'
+import express, { response } from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import cors from 'cors'
+import connectDB from './config/db.js'
+import Product from './models/productModel.js'
+import asyncHandler from 'express-async-handler'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+connectDB()
+
+const PORT = process.env.PORT || 5000
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID)
+})
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ...')
+  })
+}
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+app.listen(PORT, () =>
+  console.log(
+    `Server started on http://localhost:${PORT}`.yellow.bold.underline
+  )
+)
