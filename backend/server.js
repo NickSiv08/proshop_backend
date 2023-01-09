ddd3b0e9c34e96a48df4273935c2a7973878f847
NickSiv08 <115428555+NickSiv08@users.noreply.github.com>
2023-01-09 18:38:59 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors({ origin: 'https://proshop-shopping-cart.vercel.app' }))
+app.use(cors({ origin: '' }))
 
 connectDB()
 
